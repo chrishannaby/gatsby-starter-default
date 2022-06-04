fa69508185d2c17b836a2e364d7aa40df3028c1f
Chris Hannaby <chris.hannaby@netlify.com>
2022-06-03 18:42:25 -0600

    remove slash

--- netlify/edge-functions/proxy-requests-wildcard.ts
+++ netlify/edge-functions/proxy-requests-wildcard.ts
@@ -1,7 +1,7 @@
 import { Context } from "netlify:edge";
 
 const pathRegex = /^.*\/resources\//;
-const proxyUrl = "https://read.uberflip.com/";
+const proxyUrl = "https://read.uberflip.com";
 
 export default async (request: Request, context: Context) => {
   const path = request.url.replace(pathRegex, "");
