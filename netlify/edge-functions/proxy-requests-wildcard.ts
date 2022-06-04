cf400db644e6c5a45c6a1a36d1689b6d6739212f
Chris Hannaby <chris.hannaby@netlify.com>
2022-06-03 18:37:10 -0600

    x-forwarded

--- netlify/edge-functions/proxy-requests-wildcard.ts
+++ netlify/edge-functions/proxy-requests-wildcard.ts
@@ -1,11 +1,15 @@
 import { Context } from "netlify:edge";
 
 const pathRegex = /^.*\/resources\//;
-const proxyUrl = "https://eoglxf830gsuc1w.m.pipedream.net";
+const proxyUrl = "https://read.uberflip.com/resources-test/";
 
 export default async (request: Request, context: Context) => {
   const path = request.url.replace(pathRegex, "");
-  const response = await fetch(`${proxyUrl}/${path}`);
+  const response = await fetch(`${proxyUrl}/${path}`, {
+    headers: {
+       "X-Forwarded-Host": "adskconstruction.autodesk.com/",
+    }
+  });
   console.log(response)
   return response;
 };
