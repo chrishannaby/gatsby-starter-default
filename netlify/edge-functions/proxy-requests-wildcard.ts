ee0365e8be3097f5e054c317cddb8fdf3f4d14b9
Chris Hannaby <chris.hannaby@netlify.com>
2022-06-06 11:49:31 -0600

    change urls

--- netlify/edge-functions/proxy-requests-wildcard.ts
+++ netlify/edge-functions/proxy-requests-wildcard.ts
@@ -1,13 +1,15 @@
 import { Context } from "netlify:edge";
 
 const pathRegex = /^.*\/resources\//;
-const proxyUrl = "https://read.uberflip.com/resources-test";
+const proxyUrl = "https://read.uberflip.com/resources";
 
 export default async (request: Request, context: Context) => {
   const path = request.url.replace(pathRegex, "");
   const response = await fetch(`${proxyUrl}/${path}`, {
     headers: {
-       "X-Forwarded-Host": "adskconstruction.autodesk.com/",
+      "X-Forwarded-Host": "gatsby-edge-functions-proxy-test.netlify.app",
+      "X-Original-Host": "gatsby-edge-functions-proxy-test.netlify.app",
+      "X-Netlify-Hostname": "gatsby-edge-functions-proxy-test.netlify.app"
     }
   });
   console.log(response)
